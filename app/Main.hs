853ae03930364be5608dc421f02902b1c5e522cc
xeno <xeno.freenode@gmail.com>
2020-01-12 11:22:01 +0100

    ...

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,9 @@
 module Main where
 
-import Lib
-import Data.Char
-
-c x = ord x - ord '0'
-
-parse acc []          = [acc]
-parse acc ('\n' : xs) = parse acc []
-parse acc (',' : xs)  = acc : parse 0 xs
-parse acc (x   : xs)  = parse (acc * 10 + (c x)) xs
+import           Data.Char
+import           Input     (readProgramFromFile)
 
 main :: IO ()
 main = do
-  programString <- readFile "input.txt"
-  print programString
-  let program = parse 0 programString
+  program <- readProgramFromFile "input.txt"
   print program
-  someFunc
---
+++ src/Input.hs
@@ -0,0 +1,18 @@
+module Input (readProgramFromFile) where
+
+import Data.Char
+
+c x = ord x - ord '0'
+
+parse acc []          = [acc]
+parse acc ('\n' : xs) = parse acc []
+parse acc (',' : xs)  = acc : parse 0 xs
+parse acc (x   : xs)  = parse (acc * 10 + (c x)) xs
+
+readProgramFromFile :: string -> IO [Int]
+readProgramFromFile filename = do
+    programString <- readFile "input.txt"
+    print programString
+    return (parse 0 programString)
+
+
