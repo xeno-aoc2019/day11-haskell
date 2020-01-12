362226d4b220b765ee9b946216c2cbb105024999
xeno <xeno.freenode@gmail.com>
2020-01-12 16:56:37 +0100

    ...

--- src/Memory.hs
+++ src/Memory.hs
@@ -16,7 +16,7 @@ import           Data.Sequence
 import           Prelude         hiding (filter, lookup, map, null)
 
 data Memory =
-  Memory (Map Int Int) (Seq Int) (Seq Int)
+  Memory (Map Int Int) (Seq Int) (Seq Int) Int Bool
 
 fromProgram :: [Int] -> Memory
 store :: Memory -> Int -> Int -> Memory
@@ -25,21 +25,25 @@ addInput :: Memory -> Int -> Memory
 addOutput :: Memory -> Int -> Memory
 getOutput :: Memory -> (Int, Memory)
 getInput :: Memory -> (Int, Memory)
+hasInput :: Memory -> Bool
 instance Show Memory where
-  show (Memory m input output) = show "Memory: " ++ (show m) ++ " input=" ++ (show input) ++ " output=" ++ (show output)
+  show (Memory m input output ip halted) = show "Memory: " ++ show m ++ " input=" ++ show input ++ " output=" ++ show output
 
 fromProgram program =
   let prog = Data.Map.Strict.fromList $ indexed program
-   in Memory prog Empty Empty
+   in Memory prog Empty Empty 0 False
 
-store (Memory vm input output) position value = Memory (insert position value vm) input output
+hasInput (Memory vm Empty output ip halted) = False
+hasInput (Memory vm (x :<| xs) output ip halted) = True
 
-load (Memory vm input output) position = vm ! position
+store (Memory vm input output ip halted) position value = Memory (insert position value vm) input output ip halted
 
-getInput (Memory vm (val :<| inp) outp) = (val, Memory vm inp outp)
+load (Memory vm input output ip halted) position = vm ! position
 
-addInput (Memory vm inp outp) value = Memory vm (inp |> value) outp
+getInput (Memory vm (val :<| inp) outp ip halted) = (val, Memory vm inp outp ip halted)
 
-addOutput (Memory vm inp outp) value = Memory vm (inp |> value) outp
+addInput (Memory vm inp outp ip halted) value = Memory vm (inp |> value) outp ip halted
 
-getOutput (Memory vm inp (val :<| outp)) = (val, Memory vm inp outp)
+addOutput (Memory vm inp outp ip halted) value = Memory vm (inp |> value) outp ip halted
+
+getOutput (Memory vm inp (val :<| outp) ip halted) = (val, Memory vm inp outp ip halted)
