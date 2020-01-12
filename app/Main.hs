c1593290b73e7df0e79c444b4062a5b6c93f5a4e
xeno <xeno.freenode@gmail.com>
2020-01-12 16:42:58 +0100

    ...

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,14 @@ module Main where
 
 import           Data.Char
 import           Input     (readProgramFromFile)
-import           Memory    (fromProgram, store, load)
+import           Memory    (fromProgram, store, load, addInput, getInput, addOutput, getOutput)
 
 main :: IO ()
 main = do
   program <- readProgramFromFile "input.txt"
   let vm = fromProgram program
-  print program
-  print vm
   print $ store vm 2 1234
+  print $ load vm 1
+  let vm1 = addInput vm 10
+  let vm2 = addInput vm1 20
+  print vm2
--- src/Memory.hs
+++ src/Memory.hs
@@ -4,20 +4,29 @@ module Memory
   , Show
   , load
   , store
+  , addInput
+  , getInput
+  , addOutput
+  , getOutput
   ) where
 
 import           Data.List.Index
 import           Data.Map.Strict
-import           Prelude         hiding (filter, lookup, map, null)
 import           Data.Sequence
+import           Prelude         hiding (filter, lookup, map, null)
 
-data Memory = Memory (Map Int Int) (Seq Int) (Seq Int)
+data Memory =
+  Memory (Map Int Int) (Seq Int) (Seq Int)
 
 fromProgram :: [Int] -> Memory
 store :: Memory -> Int -> Int -> Memory
 load :: Memory -> Int -> Int
+addInput :: Memory -> Int -> Memory
+addOutput :: Memory -> Int -> Memory
+getOutput :: Memory -> (Int, Memory)
+getInput :: Memory -> (Int, Memory)
 instance Show Memory where
-  show (Memory m input output) = show "Memory: " ++ show m
+  show (Memory m input output) = show "Memory: " ++ (show m) ++ " input=" ++ (show input) ++ " output=" ++ (show output)
 
 fromProgram program =
   let prog = Data.Map.Strict.fromList $ indexed program
@@ -26,3 +35,11 @@ fromProgram program =
 store (Memory vm input output) position value = Memory (insert position value vm) input output
 
 load (Memory vm input output) position = vm ! position
+
+getInput (Memory vm (val :<| inp) outp) = (val, Memory vm inp outp)
+
+addInput (Memory vm inp outp) value = Memory vm (inp |> value) outp
+
+addOutput (Memory vm inp outp) value = Memory vm (inp |> value) outp
+
+getOutput (Memory vm inp (val :<| outp)) = (val, Memory vm inp outp)
