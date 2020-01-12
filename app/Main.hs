ec55ab438c43c2c0a3bdb361ad46a14910409caf
xeno <xeno.freenode@gmail.com>
2020-01-12 11:04:25 +0100

    parsing program

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
 import Lib
+import Data.Char
+
+c x = ord x - ord '0'
+
+parse acc []          = [acc]
+parse acc ('\n' : xs) = parse acc []
+parse acc (',' : xs)  = acc : parse 0 xs
+parse acc (x   : xs)  = parse (acc * 10 + (c x)) xs
 
 main :: IO ()
-main = someFunc
+main = do
+  programString <- readFile "input.txt"
+  print programString
+  let program = parse 0 programString
+  print program
+  someFunc
